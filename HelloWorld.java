8d9e217dd4923d0723692bb7504bef0464e6d911
Mihir Jha <mihirjha123@gmail.com>
2018-06-22 11:13:52 +0530

    Added class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
-public class Main {
+public class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
     }
+
 }
